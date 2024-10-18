739714b1cb13d8b8f879697b394ed17774dc2bea
1998-AVI <151818818+1998-AVI@users.noreply.github.com>
2024-10-18 06:22:39 +0000

    variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1264747;
+let accountEmail ="avijit@gmail.com";
+var accountPassword ="12345";
+accountCity ="jaypur";
+let accountState;
+
+accountEmail="hc@gmail.com";
+accountPassword="878679";
+accountCity="chennai";
+/*
+ var prefer not to use
+ because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
