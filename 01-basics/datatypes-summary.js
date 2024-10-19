a607c39728f6a939992d554fb5e8a92e3da282d0
1998-AVI <151818818+1998-AVI@users.noreply.github.com>
2024-10-19 10:09:39 +0000

    memory

--- 01-basics/datatypes-summary.js
+++ 01-basics/datatypes-summary.js
@@ -35,3 +35,26 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// stack(primitive data type) heap(non-primitive data type)
+
+let myYoutubename="Avijitkundudotcom";
+let anotherName="myYoutubename";
+anotherName="chaiaurcode";
+console.log(myYoutubename);
+console.log(anotherName);
+
+
+// heap memory-reference
+let userOne={
+    email:"avijit@gmail.com",
+    upiId:"user@ybl"
+}
+let userTwo=userOne;
+userTwo.email="avi@gmail.com";
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
