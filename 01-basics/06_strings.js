8987524919e4707808daa3ccb139c61c8bb4574f
1998-AVI <151818818+1998-AVI@users.noreply.github.com>
2024-10-19 10:52:51 +0000

    strings in javascript

---
+++ 01-basics/06_strings.js
@@ -0,0 +1,31 @@
+const name = "Avijit-AA"
+const countRepo = 20
+
+// console.log(`hello my name is ${name} and my countRepo ${countRepo}`);
+
+
+const gameName = new String("Avijit-hc-an");
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("j"));
+
+const newString=gameName.substring(0,4);
+console.log(newString);
+
+const anotherString= gameName.slice(1,4);
+console.log(anotherString);
+
+const newStringOne="   AVijit  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+console.log(gameName.split("-"));
+
+
+
+
+
+
+
+
